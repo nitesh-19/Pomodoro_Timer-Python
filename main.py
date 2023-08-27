341fc4de7359023cdddc28e6cb9be6391cb10f08
nitesh-19 <niteshgohil25@gmail.com>
2023-08-27 17:35:06 +0530

    Prettified UI and fixed bugs

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
+FONT_NAME = "Courier New"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-image_path = "tomato.png"
+image_path = "tomato2.png"
 time_intervals_completed = 0
 last_session = None
 
@@ -24,10 +24,8 @@ def reset():
     global timer
     window.after_cancel(timer)
     global time_intervals_completed
-
     global checkmark
     time_intervals_completed = 0
-
     canvas.itemconfig(time_text, text=f"00:00")
     label.config(text="Pomodoro", fg=GREEN)
     checkmark = []
@@ -37,7 +35,6 @@ def reset():
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
     global time_intervals_completed
-
     global loop_finish
     global timer
     global last_session
@@ -95,8 +92,8 @@ def count_down(count):
     canvas.itemconfig(time_text, text=f"{minutes_text}:{seconds_text}")
     if count > 0:
         timer = window.after(1000, count_down, count - 1)
+    loop_finish = 0
     if count == 0 and time_intervals_completed % 2 != 0:
-        loop_finish = 0
         add_checkmark()
         loop_finish = 1
 
@@ -114,20 +111,21 @@ def add_checkmark():
 
 window = Tk()
 window.title("Pomodoro Timer")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=80, pady=50, bg=YELLOW)
 fg = GREEN
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas = Canvas(width=275, height=400, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file=image_path)
-canvas.create_image(100, 112, image=tomato_img)
-time_text = canvas.create_text(100, 130, text="00:00", fill="black", font=(FONT_NAME, 35, "bold"))
+canvas.create_image(150, 250, image=tomato_img)
+time_text = canvas.create_text(140, 250, text="00:00", fill="black", font=("Calibri", 27, "bold"))
 label = Label(text="Pomodoro", bg=YELLOW, fg=fg, font=(FONT_NAME, 30, "bold"))
 start_button = Button(master=window, text="Start", command=start_timer)
 reset_button = Button(master=window, text="Reset", command=reset)
+
 label.grid(row=0, column=1)
 canvas.grid(row=1, column=1)
 start_button.grid(row=2, column=0)
 reset_button.grid(row=2, column=3)
-print_checkmark = Label(text="", bg=YELLOW, fg=fg, font=(FONT_NAME, 35, "bold"))
+print_checkmark = Label(text="", bg=YELLOW, fg=fg, font=("Courier", 35, "bold"))
 print_checkmark.grid(row=3, column=1)
 
 window.mainloop()
