0ae9523155ede5935b395380e11aed9f2b49e44a
nitesh-19 <niteshgohil25@gmail.com>
2023-08-27 18:30:41 +0530

    Updated documentation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 from tkinter import *
 
+## CREDITS TO Dr. Angela Yu for this idea from her 100 days of python course.
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -13,44 +14,60 @@ LONG_BREAK_MIN = 20
 image_path = "tomato2.png"
 time_intervals_completed = 0
 last_session = None
+fg = GREEN
 
-checkmark = []
+checkmark_list = []
 timer = None
-loop_finish = -1
+is_loop_finished = -1  # Indicates whether a Work session countdown has finished
 
 
-# ---------------------------- TIMER RESET ------------------------------- #
 def reset():
+    """
+    Resets the window to initial values
+
+    :return:
+    """
     global timer
     window.after_cancel(timer)
     global time_intervals_completed
-    global checkmark
+    global checkmark_list
     time_intervals_completed = 0
     canvas.itemconfig(time_text, text=f"00:00")
     label.config(text="Pomodoro", fg=GREEN)
     start_button.config(text="Start")
 
-    checkmark = []
-    print_checkmark.config(text="")
+    checkmark_list = []
+    checkmark.config(text="")
 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
+    """
+    Starts the timer and updates the screen.
+    :return:
+    """
     global time_intervals_completed
-    global loop_finish
+    global is_loop_finished
     global timer
     global last_session
-    global checkmark
+    global checkmark_list
+
+    # Cancel any running timers
     if time_intervals_completed > 0:
         window.after_cancel(timer)
 
-    if last_session == "Work" and loop_finish == 0:
+    # If user finished early in their Work session, update the checkmark indicating completion.
+    if last_session == "Work" and is_loop_finished == 0:
         add_checkmark()
-        loop_finish = 1
-    if len(checkmark) == 4:
-        checkmark = []
-        print_checkmark.config(text="")
+        is_loop_finished = 1  # Reset indicator
+
+    # If user finishes 4 Work sessions then reset tickmark count
+    if len(checkmark_list) == 4:
+        checkmark_list = []
+        checkmark.config(text="")
+
     seconds_to_work = {"work": WORK_MIN * 60, "break": SHORT_BREAK_MIN * 60, "long_break": LONG_BREAK_MIN * 60}
+
+    # Update UI according to the "Long Break" session parameters.
     if time_intervals_completed % 7 == 0 and time_intervals_completed != 0:
         label.config(text="Long Break", fg=PINK)
         count_down(seconds_to_work["long_break"])
@@ -58,29 +75,34 @@ def start_timer():
         last_session = "Long Break"
         time_intervals_completed += 1
 
+    # Update UI according to the "Work" session parameters.
     elif time_intervals_completed % 2 == 0:
         label.config(text="Work", fg=GREEN)
         last_session = "Work"
         count_down(seconds_to_work["work"])
         start_button.config(text="Start\nBreak")
-
         time_intervals_completed += 1
 
+    # Update UI according to the "Break" session parameters.
     elif time_intervals_completed % 2 == 1:
         label.config(text="Break", fg=RED)
         count_down(seconds_to_work["break"])
         start_button.config(text="Start\nWork")
         last_session = "Break"
-
         time_intervals_completed += 1
 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-
-
 def count_down(count):
+    """
+    Sets a countdown from 'count' value to zero.
+
+    :param count: Number of seconds
+    :return:
+    """
     global timer
-    global loop_finish
+    global is_loop_finished
+
+    # Bring text in MM:SS format
     minutes_text = math.floor(count / 60)
     seconds_text = round(count % 60)
     if seconds_text == 0:
@@ -91,44 +113,59 @@ def count_down(count):
         minutes_text = "00"
     elif minutes_text < 10:
         minutes_text = f"0{minutes_text}"
+
+    # Update screen with latest time
     canvas.itemconfig(time_text, text=f"{minutes_text}:{seconds_text}")
     if count > 0:
-        timer = window.after(1000, count_down, count - 1)
-    loop_finish = 0
+        timer = window.after(1000, count_down, count - 1)  # Recall function after one second
+    is_loop_finished = 0
+
+    # Add checkmark on screen if a Work session is finished
     if count == 0 and time_intervals_completed % 2 != 0:
         add_checkmark()
-        loop_finish = 1
+        is_loop_finished = 1
 
 
 def add_checkmark():
-    global checkmark
-    if len(checkmark) == 4:
-        checkmark = []
-    checkmark.append("🗸")
-    checkmarks_string = "".join(checkmark)
-    print_checkmark.config(text=checkmarks_string)
+    """
+    Adds a tick mark to the 'checkmark' list.
+    :return:
+    """
+    global checkmark_list
+    if len(checkmark_list) == 4:
+        checkmark_list = []
+    checkmark_list.append("🗸")
+    checkmarks_string = "".join(checkmark_list)  # Convert list to string
+    checkmark.config(text=checkmarks_string)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
-
+# Create a Tkinter Window
 window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=80, pady=50, bg=YELLOW)
-fg = GREEN
+
+# Create Canvas for background image
+
 canvas = Canvas(width=275, height=400, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file=image_path)
 canvas.create_image(150, 250, image=tomato_img)
+
+# Add timer text and buttons
 time_text = canvas.create_text(140, 250, text="00:00", fill="black", font=("Calibri", 27, "bold"))
 label = Label(text="Pomodoro", bg=YELLOW, fg=fg, font=(FONT_NAME, 30, "bold"))
 start_button = Button(master=window, text="Start", command=start_timer, font=("helvetica", 12, "bold"), bd=0, bg=GREEN,
                       fg=YELLOW, padx=5, pady=5)
 reset_button = Button(master=window, text="Reset", command=reset, font=("helvetica", 12, "bold"), bd=0, bg=GREEN,
                       fg=YELLOW, padx=5, pady=5)
+
+# Place all widgets in respective grids
 label.grid(row=0, column=1)
 canvas.grid(row=1, column=1)
 start_button.grid(row=2, column=0)
 reset_button.grid(row=2, column=3)
-print_checkmark = Label(text="", bg=YELLOW, fg=fg, font=("Courier", 35, "bold"))
-print_checkmark.grid(row=3, column=1)
+
+checkmark = Label(text="", bg=YELLOW, fg=fg, font=("Courier", 35, "bold"))
+checkmark.grid(row=3, column=1)
 
 window.mainloop()
