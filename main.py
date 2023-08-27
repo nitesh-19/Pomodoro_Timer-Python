e4e760265769da608a3e9b577ebe9801aa5668ae
nitesh-19 <niteshgohil25@gmail.com>
2023-08-27 16:58:31 +0530

    Fixed multiple timer starts on repeated button clicks, added feature to mark work session finish before time.

--- main.py
+++ main.py
@@ -12,47 +12,67 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 image_path = "tomato.png"
 time_intervals_completed = 0
-work_sessions_completed = 0
+last_session = None
+
 checkmark = []
 timer = None
+loop_finish = -1
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def reset():
+    global timer
     window.after_cancel(timer)
     global time_intervals_completed
-    global work_sessions_completed
+
     global checkmark
     time_intervals_completed = 0
-    work_sessions_completed = 0
-    checkmark = []
+
     canvas.itemconfig(time_text, text=f"00:00")
     label.config(text="Pomodoro", fg=GREEN)
+    checkmark = []
     print_checkmark.config(text="")
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
     global time_intervals_completed
-    global work_sessions_completed
 
-    seconds_to_work = {"work": 2 * 1, "break": 2 * 1, "long_break": 10 * 1}
+    global loop_finish
+    global timer
+    global last_session
+    global checkmark
+    if time_intervals_completed > 0:
+        window.after_cancel(timer)
+
+    if last_session == "Work" and loop_finish == 0:
+        add_checkmark()
+        loop_finish = 1
+    if len(checkmark) == 4:
+        checkmark = []
+        print_checkmark.config(text="")
+    seconds_to_work = {"work": 5 * 1, "break": 5 * 1, "long_break": 10 * 1}
     if time_intervals_completed % 7 == 0 and time_intervals_completed != 0:
         label.config(text="Long Break", fg=PINK)
         count_down(seconds_to_work["long_break"])
         start_button.config(text="Start Work")
+        last_session = "Long Break"
         time_intervals_completed += 1
 
     elif time_intervals_completed % 2 == 0:
         label.config(text="Work", fg=GREEN)
+        last_session = "Work"
         count_down(seconds_to_work["work"])
         start_button.config(text="Start Break")
+
         time_intervals_completed += 1
-        work_sessions_completed += 1
+
     elif time_intervals_completed % 2 == 1:
         label.config(text="Break", fg=RED)
         count_down(seconds_to_work["break"])
         start_button.config(text="Start Work")
+        last_session = "Break"
+
         time_intervals_completed += 1
 
 
@@ -61,6 +81,7 @@ def start_timer():
 
 def count_down(count):
     global timer
+    global loop_finish
     minutes_text = math.floor(count / 60)
     seconds_text = round(count % 60)
     if seconds_text == 0:
@@ -71,12 +92,13 @@ def count_down(count):
         minutes_text = "00"
     elif minutes_text < 10:
         minutes_text = f"0{minutes_text}"
-
     canvas.itemconfig(time_text, text=f"{minutes_text}:{seconds_text}")
     if count > 0:
         timer = window.after(1000, count_down, count - 1)
     if count == 0 and time_intervals_completed % 2 != 0:
+        loop_finish = 0
         add_checkmark()
+        loop_finish = 1
 
 
 def add_checkmark():
@@ -103,9 +125,9 @@ start_button = Button(master=window, text="Start", command=start_timer)
 reset_button = Button(master=window, text="Reset", command=reset)
 label.grid(row=0, column=1)
 canvas.grid(row=1, column=1)
-start_button.grid(row=3, column=0)
-reset_button.grid(row=3, column=3)
-print_checkmark = Label(text="", bg=YELLOW, fg=fg, font=(FONT_NAME, 25, "bold"))
-print_checkmark.grid(row=2, column=1)
+start_button.grid(row=2, column=0)
+reset_button.grid(row=2, column=3)
+print_checkmark = Label(text="", bg=YELLOW, fg=fg, font=(FONT_NAME, 35, "bold"))
+print_checkmark.grid(row=3, column=1)
 
 window.mainloop()
